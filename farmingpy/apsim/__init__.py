29e0515711eb86b22f073615d513b8c98448caad
Matti Pastell <430863+mpastell@users.noreply.github.com>
2024-09-13 13:00:32 +0000

    Update imports

--- farmingpy/apsim/__init__.py
+++ farmingpy/apsim/__init__.py
@@ -3,12 +3,17 @@ try:
 except:
     print("Failed to load APSIMX, see https://github.com/TwinYields/farmingpy?tab=readme-ov-file#apsim-interface")
 
+try:
+    from .ensemble import APSIMXEnsemble, Report, Fertilizer
+except:
+    print("Failed to load Ensemble methods, a custom build of APSIM is required")
+  
 from .metfiles import read_met
 
 # This will fail without a custom build of APSIM
 # TODO catch only C# import errors
 try:
     from .optimizer import SoilOptimizer, PhenologyOptimizer, OptimizerBase
-    from .ensemble import APSIMXEnsemble, Report, Fertilizer
 except:
-    pass
+    print("Failed to load optimizers, please install nlopt")
+    
