014d38772b43fc9c737cdc1fe826a5ed466fc2e2
hartian <hartikainenannimari@gmail.com>
2025-09-23 17:35:25 +0300

    remove unnecessary class layer from SmartDataModels types

--- farmingpy/datamodel/dm_scripts.py
+++ farmingpy/datamodel/dm_scripts.py
@@ -13,12 +13,22 @@ def pyobj_to_ngsild(pyobj):
     for key, value in pydump.items():
         if key == 'agriparcel':
             ngsidump[key] = {'object':value, 'type':'Relationship'}
-        elif key == 'id' or key == 'type':
+        elif key == 'id':
             ngsidump[key] = value
+        elif key == 'type':
+            if isinstance(value, str):
+                ngsidump[key] = value
+            else:
+                ngsidump[key] = value.value # Remove type description coming from SmartDataModels
         elif key == 'location':
             #value_geojson = shapely.to_geojson(value) # uncomment to use shapely geometry instead of geojson
             #value_geojson_dict = json.loads(value_geojson) # uncomment to use shapely geometry instead of geojson
-            ngsidump[key] = {'value':value, 'type':'GeoProperty'}
+            if isinstance(value['type'], str):
+                ngsidump[key] = {'value':value, 'type':'GeoProperty'}
+            else:
+                value_with_str_type = value['type'].value # Remove type description coming from SmartDataModels
+                value['type'] = value_with_str_type
+                ngsidump[key] = {'value':value, 'type':'GeoProperty'}
         elif isinstance(value, datetime.date): 
             datetime_str = value.isoformat() # NGSI-LD requires date as a string in ISO 8601 format
             ngsidump[key] = {'value':datetime_str, 'type':'Property'}
